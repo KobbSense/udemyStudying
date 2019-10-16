516857aa7f024ca506d552022a5477d3a4559313
KobbSense <55086810+KobbSense@users.noreply.github.com>
2019-10-16 15:22:08 +0200

    Add files via upload
    First commit

---
+++ udemy/script00.js
@@ -0,0 +1,49 @@
+'use strict'
+
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(leftBorderWidth);
+console.log("Hello" + '\n' + second);
+console.log(4 / 0)
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("Hello World!");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+//
+// let answer = +prompt("Есть-ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log(4 +  +" - object");
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++ + "\n" + decr--);
+
+console.log(5 % 2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
