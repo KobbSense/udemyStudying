00bfa7bdb7e40021e44efcab1454cfbe81e18d60
KobbSense <alexvitaly92@gmail.com>
2019-10-25 16:40:38 +0200

    10.25.2019(1)

--- udemy/udemi19/script.js
+++ udemy/udemi19/script.js
@@ -3,6 +3,7 @@ let box = document.getElementById('box'),
     circle = document.getElementsByClassName('circle'),
     heart = document.querySelectorAll('.heart'),
     oneHeart = document.querySelector('.heart');
+    wrapper = document.querySelector(".wrapper");
 
 box.style.backgroundColor = 'blue';
 btn[1].style.borderRadius = '100%';
@@ -12,6 +13,36 @@ circle[1].style.backgroundColor = 'yellow';
 //circle[1].style.borderRadius = '50%';
 circle[2].style.backgroundColor = 'green';
 
-for (let i = 0; i < heart.length; i++) {
+/* for (let i = 0; i < heart.length; i++) {
     heart[i].style.backgroundColor = 'blue';
-}
+} */
+
+/* heart.forEach(function (item, i, hearts) {
+    item.style.backgroundColor = "blue";
+}); */
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+/* document.body.appendChild(div); */
+/* wrapper.appendChild(div); */
+
+/* div.innerHTML = '<h1>Hello World!</h1>'; */
+div.textContent = "Hello World!";
+
+
+
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+/* wrapper.removeChild(heart[1]); */
+
+document.body.replaceChild(btn[1], circle[1]);
+
+    console.log(div);
+
+
+
+
+
